fa703edd94eb57ebe93a2b948b44589cf83c3d67
Nikita Zhukov <145158875+ProudRykar@users.noreply.github.com>
2024-09-16 16:29:50 +0300

    Add files via upload

--- labs/lab1/Palindrome.java
+++ labs/lab1/Palindrome.java
@@ -1,24 +1,32 @@
 package labs.lab1;
 
+import java.util.Scanner;
+
 public class Palindrome {
-  public static void main(String[] args) {
-    for (int i = 0; i < args.length; i++) {
-        String s = args[i];
-        if (isPalindrome(s)) {
-            System.out.println(s + " is a palindrome");
-        } else {
-            System.out.println(s + " is not a palindrome");
-        }
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in); 
+      System.out.print("Введите слово: ");
+      String input = scanner.nextLine();
+      String input_changed = input.replaceAll("\\s+", "").toLowerCase();
+      
+      if (isPalindrome(input_changed)) {
+          System.out.println(input + " является палиндромом");
+      } else {
+          System.out.println(input + " не является палиндромом");
+      }
+      
+      scanner.close(); 
     }
-}
-public static String reverseString(String s) {
+
+  public static String reverseString(String s) {
     String reversed = "";
     for (int i = s.length() - 1; i >= 0; i--) {
         reversed += s.charAt(i);
     }
     return reversed;
-}
-public static boolean isPalindrome(String s) {
+  }
+
+  public static boolean isPalindrome(String s) {
     return s.equals(reverseString(s));
-}
+  }
 }
